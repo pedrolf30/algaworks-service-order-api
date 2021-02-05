e8171a06f2afb413bcb3a0f71595bfdbbb00eccc
pedrolf30 <pedrolf30@gmail.com>
2021-02-05 07:30:58 -0300

    Database connection and migration with flyway

--- src/main/java/com/example/osworks/osworksapi/OsworksApiApplication.java
+++ src/main/java/com/example/osworks/osworksapi/OsworksApiApplication.java
@@ -3,6 +3,8 @@ package com.example.osworks.osworksapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//gracious_dubinsky
+
 @SpringBootApplication
 public class OsworksApiApplication {
 
